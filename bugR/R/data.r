3d905de921dd3a61839326b66e8667c26481c635
Jeffrey D. Muehlbauer <jmuehlbauer@usgs.gov>
2016-07-12 10:03:48 -0700

    Documented embedded datasets.

---
+++ bugR/R/data.r
@@ -0,0 +1,14 @@
+#' A simple matrix for ordination.
+#'
+#' @format A data frame with 20 rows representing sites and 5 columns representing species.
+"ordmat"
+
+#' 2-dimensional ordination output.
+#'
+#' @format A dataframe with 20 rows representing sites and 2 columns representing ordination axes.
+"pts2D"
+
+#' A simple environmental variable dataframe.
+#'
+#' @format A data frame with 20 rows representing sites and 2 columns representing environmental variables.
+"evar"
