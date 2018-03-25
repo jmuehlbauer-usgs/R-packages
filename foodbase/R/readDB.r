434eef3ba765fcb34414acbce6b650f02f6e6e3a
mdodrill-usgs <mdodrill@usgs.gov>
2018-03-25 08:34:20 -0700

    Working changes to allow for 'FishGut'

--- foodbase/R/readDB.r
+++ foodbase/R/readDB.r
@@ -13,7 +13,7 @@
 #'
 #' Regardless of the \code{updater} setting, \code{readDB} checks for a local copy of the \code{Sample}, \code{Specimen}, and \code{SppList} data in the \code{Data} folder of the \code{foodbase} package, and will install these three files there if they are not present (these local copies are used for other functions within the \code{foodbase} package). Using \code{updater} will update all three of these files, regardless of what data type is specified by \code{type}.
 #'
-#' If not set to \code{FALSE}, the \code{updater} can be set to download new data from the \code{Network} or from \code{GitHub}. However, it's generally better to specify only \code{updater = TRUE}, in which case R will check to see if you are connected to the DOI network and will download data from the Network if so and from GitHub if not. Although data are pushed to GitHub nightly, the Network data are almost always the most current. 
+#' If not set to \code{FALSE}, the \code{updater} can be set to download new data from the \code{Network} or from \code{GitHub}. However, it's generally better to specify only \code{updater = TRUE}, in which case R will check to see if you are connected to the DOI network and will download data from the Network if so and from GitHub if not. Although data are pushed to GitHub nightly, the Network data are almost always the most current.
 
 #' @return Creates a dataframe containing the desired data from the Foodbase database.
 
@@ -52,24 +52,29 @@ readDB <- function(gear = "Drift", type = "Sample", updater = FALSE){
   if(dbdir.exists == FALSE | updater != FALSE){
 	netcheck <- length(grep('gs.doi.net', system('ipconfig', intern = TRUE)))
     if(updater == 'GitHub' | netcheck == 0){
-      path <- paste0('https://raw.githubusercontent.com/jmuehlbauer-usgs/Database/master/')
+      # path <- paste0('https://raw.githubusercontent.com/jmuehlbauer-usgs/Database/master/')   # change when done with Gut update
+      path <- paste0('C:/Users/mdodrill/Desktop/FB_Git/R-packages/foodbase/data_temp/')
     } else {
-      path <- paste0('P:/BIOLOGICAL/Foodbase/Database/Exports/')
+      # path <- paste0('P:/BIOLOGICAL/Foodbase/Database/Exports/')                 # change when done with Gut update
+      path <- paste0('C:/Users/mdodrill/Desktop/FB_Git/R-packages/foodbase/data_temp/')
     }
     dir.create(dbdir, showWarnings = FALSE)
-    samp <- read.csv(paste0(path, gear, 'Sample.csv'))
-    spec <- read.csv(paste0(path, gear, 'Specimen.csv'))
-    sppl <- read.csv(paste0(path, 'SppList.csv'))
+    # samp <- read.csv(paste0(path, gear, 'Sample.csv'))  # change when done with Gut update
+    samp <- read.csv(paste0(path, gear, 'Sample_new.csv'))
+    # spec <- read.csv(paste0(path, gear, 'Specimen.csv'))   # change when done with Gut update
+    spec <- read.csv(paste0(path, gear, 'Specimen_new.csv'))
+    sppl <- read.csv(paste0(path, 'SpeciesList.csv'))
     write.csv(samp,paste0(dbdir,'/', gear, 'Sample.csv'), row.names = FALSE)
     write.csv(spec,paste0(dbdir,'/', gear, 'Specimen.csv'), row.names = FALSE)
-    write.csv(sppl,paste0(dbdir,'/', 'SppList.csv'), row.names = FALSE)
+    write.csv(sppl,paste0(dbdir,'/', 'SpeciesList.csv'), row.names = FALSE)
   }
 
   # Read in the data
   if(type == 'SppList'){
-    dat <- read.csv(paste0(dbdir, '/SppList.csv'))
+    dat <- read.csv(paste0(dbdir, '/SpeciesList.csv'))
   } else {
     dat <- read.csv(paste0(dbdir, '/', gear, type, '.csv'))
+    # dat <- read.csv(paste0(dbdir, '/', gear, type, '_new.csv'))                  # change when done with Gut update
     if(type == 'Sample'){
       dat$Date <- as.Date(dat$Date, format = '%m/%d/%Y')
       dat$ProcessDate <- as.Date(dat$ProcessDate, format = '%m/%d/%Y')
