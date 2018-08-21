a1a3726b3ee0b2ed3732abd2ff0d2694dce63fd8
Jeffrey D. Muehlbauer <jmuehlbauer@usgs.gov>
2018-08-20 17:08:07 -0700

    Typo fix.

--- foodbase/R/readDB.r
+++ foodbase/R/readDB.r
@@ -101,6 +101,6 @@ readDB <- function(gear = "Drift", type = "Sample", updater = FALSE){
   spec1 <- file.mtime(paste0(dbdir, '/', gear, 'Specimen.csv'))
   sppl1 <- file.mtime(paste0(dbdir, '/', gear, 'Specimen.csv'))
   cat(paste0('Data obtained. Dates last modified:\n', gear, ' Samples: ', samp1,
-    '\n', gear, ' Specimens: ', spec1, '\nSpecies List: ', sppl1))
+    '\n', gear, ' Specimens: ', spec1, '\nSpecies List: ', sppl1, '\n'))
   return(dat)
 }
