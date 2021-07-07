22fed900bf7db91786dd5cc89e5e7c94c56913d5
Jeffrey D. Muehlbauer <jmuehlbauer@usgs.gov>
2021-07-07 10:41:43 -0800

    Added drift mutliplier data.

---
+++ foodbase/R/driftMult-data.r
@@ -0,0 +1,18 @@
+#' Size bin and mesh size multiplers for comparing drift samples with differing mesh size.
+#'
+#' Data on the proportion of total sample counts represented by each size bin for drift samples.
+#' Also the multiplers used that vary from 250 to 500 um mesh nets.
+#' Used internally for converting to/from 250 and 500 um mesh nets.
+#'
+#' @docType data
+#'
+#' @usage data(driftMultipliers)
+#'
+#' @format An object of class \code{"list"}.
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(driftMultipliers)
+#' foo <- driftMult
+"driftMult"
